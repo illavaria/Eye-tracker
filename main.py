5c1b09dc3506874cbea6d48cdb44b96f480b421c
samal.illaria <samal.illaria@student.ehu.lt>
2024-02-28 11:59:20 +0300

    Added class eye that stores coordinates of an eye and draws it. In EyesDetector coordinates for left and right eyes are calculated and stored separately

--- main.py
+++ main.py
@@ -3,64 +3,70 @@ import mediapipe as mp
 import numpy as np
 
 
+class Eye:
+    center_x, center_y = 0, 0
+    inside_x, inside_y = 0, 0
+    outside_x, outside_y = 0, 0
+    top_x, top_y = 0, 0
+    bottom_x, bottom_y = 0, 0
+    
+    def draw(self, frame):
+        cv2.drawMarker(frame, (self.center_x, self.center_y), (0, 255, 0), markerSize=5)
+        cv2.drawMarker(frame, (self.inside_x, self.inside_y), (255, 0, 255), markerSize=5)
+        cv2.drawMarker(frame, (self.outside_x, self.outside_y), (255, 0, 255), markerSize=5)
+        cv2.drawMarker(frame, (self.top_x, self.top_y), (255, 0, 255), markerSize=5)
+        cv2.drawMarker(frame, (self.bottom_x, self.bottom_y), (255, 0, 255), markerSize=5)
+        return frame
+    
+
 class EyesDetector:
     face_mesh = mp.solutions.face_mesh.FaceMesh(static_image_mode=False,
                                                 refine_landmarks=True,
                                                 max_num_faces=2,
                                                 min_detection_confidence=0.5)
+    left_eye = Eye()
+    right_eye = Eye()
 
-    last_x = 0
-    last_y = 0
-
-    def GetFaceMeshResults(self, frame):
+    def get_face_mesh_results(self, frame):
         return self.face_mesh.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
-    def GetEyesCoordinates(self, results, frame):
+    def get_eyes_coordinates(self, results, frame):
         frame_h, frame_w, _ = frame.shape
         for face_landmarks in results.multi_face_landmarks:
-
-            # Eye center landmarks
-            rcx, rcy, lcx, lcy = 0, 0, 0, 0
-
-            # Eye corner landmarks
-            inside_x, outside_x = 0, 0
-
-            # Eye top/bottom landmarks
-            top, bottom = 0, 0
-
             # Iterate over all landmark
             for idx, lm in enumerate(face_landmarks.landmark):
-                # !!! ONLY WORKS FOR ONE EYE, FIXED FOR IN HEADPOSE.PY ITERATION !!!
 
                 # Right iris center landmark
                 if idx == 468:
-                    rcx, rcy = int(lm.x * frame_w), int(lm.y * frame_h)
-                    cv2.drawMarker(frame, (rcx, rcy), (0, 255, 0), markerSize=5)
+                    self.right_eye.center_x, self.right_eye.center_y = int(lm.x * frame_w), int(lm.y * frame_h)
 
                 # Left iris center landmark
                 if idx == 473:
-                    lcx, lcy = int(lm.x * frame_w), int(lm.y * frame_h)
-                    cv2.drawMarker(frame, (lcx, lcy), (0, 255, 0), markerSize=5)
+                    self.left_eye.center_x, self.left_eye.center_y = int(lm.x * frame_w), int(lm.y * frame_h)
 
-                    # Eye inside corner landmark
-                if idx == 133 or idx == 362: # for left eye 155 is a little bit to the left
-                    inside_x, inside_y = int(lm.x * frame_w), int(lm.y * frame_h)
-                    cv2.drawMarker(frame, (inside_x, inside_y), (255, 0, 255), markerSize=5)
+                # Eye inside corner landmark
+                if idx == 133:
+                    self.right_eye.inside_x, self.right_eye.inside_y = int(lm.x * frame_w), int(lm.y * frame_h)
+                if idx == 362:
+                    self.left_eye.inside_x, self.left_eye.inside_y = int(lm.x * frame_w), int(lm.y * frame_h)
 
-                    # Eye outside corner landmark
-                if idx == 33 or idx == 263: # for left eye 130 is like the corner of skin, 33 of purpil, 466 for right
-                    outside_x, outside_y = int(lm.x * frame_w), int(lm.y * frame_h)
-                    cv2.drawMarker(frame, (outside_x, outside_y), (255, 0, 255), markerSize=5)
+                # Eye outside corner landmark
+                if idx == 33:
+                    self.right_eye.outside_x, self.right_eye.outside_y = int(lm.x * frame_w), int(lm.y * frame_h)
+                if idx == 263: # for left eye 130 is like the corner of skin, 33 of purpil, 466 for right
+                    self.left_eye.outside_x, self.left_eye.outside_y = int(lm.x * frame_w), int(lm.y * frame_h)
 
                 # Left/right Top eye landmark
-                if idx == 159 or idx == 386:
-                    top_x, top = int(lm.x * frame_w), int(lm.y * frame_h)
-                    cv2.drawMarker(frame, (top_x, top), (255, 0, 255), markerSize=5)
+                if idx == 159:
+                    self.right_eye.top_x, self.right_eye.top_y = int(lm.x * frame_w), int(lm.y * frame_h)
+                if idx == 386:
+                    self.left_eye.top_x, self.left_eye.top_y = int(lm.x * frame_w), int(lm.y * frame_h)
 
-                    # Left/right Bottom eye landmark
-                if idx == 145 or idx == 374:
-                    bottom_x, bottom = int(lm.x * frame_w), int(lm.y * frame_h)
-                    cv2.drawMarker(frame, (bottom_x, bottom), (255, 0, 255), markerSize=5)
+                # Left/right Bottom eye landmark
+                if idx == 145:
+                    self.right_eye.bottom_x, self.right_eye.bottom_y = int(lm.x * frame_w), int(lm.y * frame_h)
+                if idx == 374:
+                    self.left_eye.bottom_x, self.left_eye.bottom_y = int(lm.x * frame_w), int(lm.y * frame_h)
 
             # # Calculate left eye scores (x,y)
             # if (inside_x - outside_x) != 0:
@@ -69,15 +75,13 @@ class EyesDetector:
             #         cv2.putText(frame, "x: {:.02}".format(lx_score), (lcx, lcy - 30), cv2.FONT_HERSHEY_SIMPLEX, .25,
             #                     (255, 255, 255), 1)
             #     last_x = lx_score
-            #
+            # 
             # if (bottom - top) != 0:
             #     ly_score = (lcy - top) / (bottom - top)
             #     if abs(ly_score - self.last_y) < .3:
             #         cv2.putText(frame, "y: {:.02}".format(ly_score), (lcx, lcy - 20), cv2.FONT_HERSHEY_SIMPLEX, .25,
             #                     (255, 255, 255), 1)
             #     last_y = ly_score
-        return frame
-
 
 
 def main():
@@ -91,7 +95,7 @@ def main():
         # To improve performance
         frame.flags.writeable = False
 
-        results = eyeDetector.GetFaceMeshResults(frame)
+        results = eyeDetector.get_face_mesh_results(frame)
 
         # Make it writeable again
         frame.flags.writeable = True
@@ -101,7 +105,9 @@ def main():
         if not results.multi_face_landmarks:
             continue
 
-        result_frame = eyeDetector.GetEyesCoordinates(results, frame)
+        eyeDetector.get_eyes_coordinates(results, frame)
+        frame_right_eye = eyeDetector.right_eye.draw(frame)
+        result_frame = eyeDetector.left_eye.draw(frame_right_eye)
 
         result_frame = cv2.flip(result_frame, 1)
         # Show the image
