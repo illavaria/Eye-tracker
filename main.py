7fc3ac88dff84a765c929c8bd76e8cbee9150596
samal.illaria <samal.illaria@student.ehu.lt>
2024-02-28 11:23:47 +0300

    Finds 5 elements of the eye

--- main.py
+++ main.py
@@ -1,95 +1,117 @@
-import mediapipe as mp
-from mediapipe import solutions
-from mediapipe.framework.formats import landmark_pb2
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
-import numpy as np
-import matplotlib.pyplot as plt
 import cv2
-import requests
+import mediapipe as mp
+import numpy as np
 
 
-def draw_landmarks_on_image(rgb_image, detection_result):
-  face_landmarks_list = detection_result.face_landmarks
-  annotated_image = np.copy(rgb_image)
+class EyesDetector:
+    face_mesh = mp.solutions.face_mesh.FaceMesh(static_image_mode=False,
+                                                refine_landmarks=True,
+                                                max_num_faces=2,
+                                                min_detection_confidence=0.5)
 
-  # Loop through the detected faces to visualize.
-  for idx in range(len(face_landmarks_list)):
-    face_landmarks = face_landmarks_list[idx]
+    last_x = 0
+    last_y = 0
 
-    # Draw the face landmarks.
-    face_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-    face_landmarks_proto.landmark.extend([
-      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in face_landmarks
-    ])
+    def GetFaceMeshResults(self, frame):
+        return self.face_mesh.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
-    solutions.drawing_utils.draw_landmarks(
-        image=annotated_image,
-        landmark_list=face_landmarks_proto,
-        connections=mp.solutions.face_mesh.FACEMESH_TESSELATION,
-        landmark_drawing_spec=None,
-        connection_drawing_spec=mp.solutions.drawing_styles
-        .get_default_face_mesh_tesselation_style())
-    solutions.drawing_utils.draw_landmarks(
-        image=annotated_image,
-        landmark_list=face_landmarks_proto,
-        connections=mp.solutions.face_mesh.FACEMESH_CONTOURS,
-        landmark_drawing_spec=None,
-        connection_drawing_spec=mp.solutions.drawing_styles
-        .get_default_face_mesh_contours_style())
-    solutions.drawing_utils.draw_landmarks(
-        image=annotated_image,
-        landmark_list=face_landmarks_proto,
-        connections=mp.solutions.face_mesh.FACEMESH_IRISES,
-          landmark_drawing_spec=None,
-          connection_drawing_spec=mp.solutions.drawing_styles
-          .get_default_face_mesh_iris_connections_style())
+    def GetEyesCoordinates(self, results, frame):
+        frame_h, frame_w, _ = frame.shape
+        for face_landmarks in results.multi_face_landmarks:
 
-  return annotated_image
+            # Eye center landmarks
+            rcx, rcy, lcx, lcy = 0, 0, 0, 0
 
-def plot_face_blendshapes_bar_graph(face_blendshapes):
-  # Extract the face blendshapes category names and scores.
-  face_blendshapes_names = [face_blendshapes_category.category_name for face_blendshapes_category in face_blendshapes]
-  face_blendshapes_scores = [face_blendshapes_category.score for face_blendshapes_category in face_blendshapes]
-  # The blendshapes are ordered in decreasing score value.
-  face_blendshapes_ranks = range(len(face_blendshapes_names))
+            # Eye corner landmarks
+            inside_x, outside_x = 0, 0
 
-  fig, ax = plt.subplots(figsize=(12, 12))
-  bar = ax.barh(face_blendshapes_ranks, face_blendshapes_scores, label=[str(x) for x in face_blendshapes_ranks])
-  ax.set_yticks(face_blendshapes_ranks, face_blendshapes_names)
-  ax.invert_yaxis()
+            # Eye top/bottom landmarks
+            top, bottom = 0, 0
 
-  # Label each bar with values
-  for score, patch in zip(face_blendshapes_scores, bar.patches):
-    plt.text(patch.get_x() + patch.get_width(), patch.get_y(), f"{score:.4f}", va="top")
+            # Iterate over all landmark
+            for idx, lm in enumerate(face_landmarks.landmark):
+                # !!! ONLY WORKS FOR ONE EYE, FIXED FOR IN HEADPOSE.PY ITERATION !!!
 
-  ax.set_xlabel('Score')
-  ax.set_title("Face Blendshapes")
-  plt.tight_layout()
-  plt.show()
+                # Right iris center landmark
+                if idx == 468:
+                    rcx, rcy = int(lm.x * frame_w), int(lm.y * frame_h)
+                    cv2.drawMarker(frame, (rcx, rcy), (0, 255, 0), markerSize=5)
+
+                # Left iris center landmark
+                if idx == 473:
+                    lcx, lcy = int(lm.x * frame_w), int(lm.y * frame_h)
+                    cv2.drawMarker(frame, (lcx, lcy), (0, 255, 0), markerSize=5)
+
+                    # Eye inside corner landmark
+                if idx == 133 or idx == 362: # for left eye 155 is a little bit to the left
+                    inside_x, inside_y = int(lm.x * frame_w), int(lm.y * frame_h)
+                    cv2.drawMarker(frame, (inside_x, inside_y), (255, 0, 255), markerSize=5)
+
+                    # Eye outside corner landmark
+                if idx == 33 or idx == 263: # for left eye 130 is like the corner of skin, 33 of purpil, 466 for right
+                    outside_x, outside_y = int(lm.x * frame_w), int(lm.y * frame_h)
+                    cv2.drawMarker(frame, (outside_x, outside_y), (255, 0, 255), markerSize=5)
+
+                # Left/right Top eye landmark
+                if idx == 159 or idx == 386:
+                    top_x, top = int(lm.x * frame_w), int(lm.y * frame_h)
+                    cv2.drawMarker(frame, (top_x, top), (255, 0, 255), markerSize=5)
+
+                    # Left/right Bottom eye landmark
+                if idx == 145 or idx == 374:
+                    bottom_x, bottom = int(lm.x * frame_w), int(lm.y * frame_h)
+                    cv2.drawMarker(frame, (bottom_x, bottom), (255, 0, 255), markerSize=5)
+
+            # # Calculate left eye scores (x,y)
+            # if (inside_x - outside_x) != 0:
+            #     lx_score = (lcx - outside_x) / (inside_x - outside_x)
+            #     if abs(lx_score - self.last_x) < .3:
+            #         cv2.putText(frame, "x: {:.02}".format(lx_score), (lcx, lcy - 30), cv2.FONT_HERSHEY_SIMPLEX, .25,
+            #                     (255, 255, 255), 1)
+            #     last_x = lx_score
+            #
+            # if (bottom - top) != 0:
+            #     ly_score = (lcy - top) / (bottom - top)
+            #     if abs(ly_score - self.last_y) < .3:
+            #         cv2.putText(frame, "y: {:.02}".format(ly_score), (lcx, lcy - 20), cv2.FONT_HERSHEY_SIMPLEX, .25,
+            #                     (255, 255, 255), 1)
+            #     last_y = ly_score
+        return frame
+
+
 
 def main():
-    # STEP 2: Create an FaceLandmarker object.
-    model_file = open('face_landmarker.task', "rb")
-    model_data = model_file.read()
-    model_file.close()
+    cap = cv2.VideoCapture(0)
+    ret, frame = cap.read()
+    eyeDetector = EyesDetector()
 
-    base_options = python.BaseOptions(model_asset_buffer=model_data)
-    options = vision.FaceLandmarkerOptions(base_options=base_options,
-                                           output_face_blendshapes=True,
-                                           output_facial_transformation_matrixes=True,
-                                           num_faces=1)
-    detector = vision.FaceLandmarker.create_from_options(options)
+    while True:
+        ret, frame = cap.read()
 
-    # STEP 3: Load the input image.
-    image = mp.Image.create_from_file("image.jpeg")
+        # To improve performance
+        frame.flags.writeable = False
 
-    # STEP 4: Detect face landmarks from the input image.
-    detection_result = detector.detect(image)
+        results = eyeDetector.GetFaceMeshResults(frame)
 
-    # STEP 5: Process the detection result. In this case, visualize it.
-    annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
-    cv2.imshow(cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
-    cv2.waitKey(0)
+        # Make it writeable again
+        frame.flags.writeable = True
+
+        frame_h, frame_w, _ = frame.shape
+
+        if not results.multi_face_landmarks:
+            continue
+
+        result_frame = eyeDetector.GetEyesCoordinates(results, frame)
+
+        result_frame = cv2.flip(result_frame, 1)
+        # Show the image
+        cv2.imshow('eyes', result_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
 
 main()
