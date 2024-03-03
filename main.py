61f07fc5c583d157c7c6ed5af8d6ef23e2c4870f
samal.illaria <samal.illaria@student.ehu.lt>
2024-03-03 10:56:59 +0300

    Added function that calculates the difference between 2 frames (doesn't need main)

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ class EyesDetector:
         return self.face_mesh.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
     def get_eyes_coordinates(self, results, frame):
-        frame_h, frame_w, _ = frame.shape
+        frame_h, frame_w = frame.shape
         for face_landmarks in results.multi_face_landmarks:
             self.right_eye.center_x, self.right_eye.center_y = int(face_landmarks.landmark[468].x * frame_w), int(face_landmarks.landmark[468].y * frame_h)
             self.left_eye.center_x, self.left_eye.center_y = int(face_landmarks.landmark[473].x * frame_w), int(face_landmarks.landmark[473].y * frame_h)
@@ -40,7 +40,7 @@ class EyesDetector:
             self.right_eye.outside_x, self.right_eye.outside_y = int(face_landmarks.landmark[33].x * frame_w), int(face_landmarks.landmark[33].y * frame_h)
             self.left_eye.outside_x, self.left_eye.outside_y = int(face_landmarks.landmark[263].x * frame_w), int(face_landmarks.landmark[263].y * frame_h)
             self.right_eye.top_x, self.right_eye.top_y = int(face_landmarks.landmark[159].x * frame_w), int(face_landmarks.landmark[159].y * frame_h)
-            self.left_eye.top_x, self.left_eye.top_y= int(face_landmarks.landmark[386].x * frame_w), int(face_landmarks.landmark[386].y * frame_h)
+            self.left_eye.top_x, self.left_eye.top_y = int(face_landmarks.landmark[386].x * frame_w), int(face_landmarks.landmark[386].y * frame_h)
             self.right_eye.bottom_x, self.right_eye.bottom_y = int(face_landmarks.landmark[145].x * frame_w), int(face_landmarks.landmark[145].y * frame_h)
             self.left_eye.bottom_x, self.left_eye.bottom_y = int(face_landmarks.landmark[374].x * frame_w), int(face_landmarks.landmark[374].y * frame_h)
 
@@ -95,6 +95,9 @@ class CalibrationData:
 def PreDefine(eyeDetector):
     cap = cv2.VideoCapture(0)
     ret, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    _, _, frame = cv2.split(hsv)
+    cv2.imwrite('frame2.jpg', frame)
     results = eyeDetector.get_face_mesh_results(frame)
     eyeDetector.get_eyes_coordinates(results, frame)
     eye_difference = eyeDetector.left_eye.outside_x- eyeDetector.right_eye.outside_x
@@ -102,6 +105,81 @@ def PreDefine(eyeDetector):
     eye_left_x = eyeDetector.left_eye.outside_x - eyeDetector.left_eye.inside_x
     cap.release()
     return eye_difference, eye_right_x, eye_left_x
+
+def Contrast(image):
+    # image = cv2.imread('/Users/illaria/BSUIR/Diploma/code/MediaPipeTry1/left_eye3/result0.jpg', 1)
+    # cv2.imshow("Original image", image)
+
+    # CLAHE (Contrast Limited Adaptive Histogram Equalization)
+    clahe = cv2.createCLAHE(clipLimit=60., tileGridSize=(6, 6))
+
+    lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)  # convert from BGR to LAB color space
+    l, a, b = cv2.split(lab)  # split on 3 different channels
+
+    l2 = clahe.apply(l)  # apply CLAHE to the L-channel
+
+    lab = cv2.merge((l2, a, b))  # merge channels
+    img2 = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)  # convert from LAB to BGR
+    return img2
+    # cv2.imshow('Increased contrast', img2)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+def Fixate():
+    n = 5
+    # image0 = cv2.imread('/Users/illaria/BSUIR/Diploma/code/MediaPipeTry1/left_eye_v_without_glasses/result0' + '.jpg',1)
+    cap = cv2.VideoCapture(0)
+    eyeDetector = EyesDetector()
+
+    ret, image0 = cap.read()
+    ret, image0 = cap.read()
+    ret, image0 = cap.read()
+    hsv = cv2.cvtColor(image0, cv2.COLOR_BGR2HSV)
+    _, _, image0 = cv2.split(hsv)
+    cv2.imwrite('frame.jpg', image0)
+
+    results = eyeDetector.get_face_mesh_results(image0)
+    eyeDetector.get_eyes_coordinates(results, image0)
+    x0, y0 = eyeDetector.left_eye.outside_x, eyeDetector.left_eye.outside_y
+
+    # image1 = cv2.imread('/Users/illaria/BSUIR/Diploma/code/MediaPipeTry1/left_eye_v_without_glasses/result1' + '.jpg', 1)
+    ret, image1 = cap.read()
+    hsv1 = cv2.cvtColor(image1, cv2.COLOR_BGR2HSV)
+    _, _, image1 = cv2.split(hsv1)
+
+
+    results = eyeDetector.get_face_mesh_results(image1)
+    eyeDetector.get_eyes_coordinates(results, image1)
+    x1, y1 = eyeDetector.left_eye.outside_x, eyeDetector.left_eye.outside_y
+    diff = np.zeros((n, n),dtype = np.int_)
+
+    image0 = image0.astype(np.int16)
+    image1 = image1.astype(np.int16)
+
+    halfn = round(n/2)
+    x, y = y0 - halfn, x0 - halfn
+    print('image0: ', x0, y0)
+    print('image1: ', x1, y1)
+
+
+    for a in range(-halfn, halfn+1):
+        for b in range(-halfn, halfn+1):
+            for i in range(0, n):
+                for j in range(0, n):
+                    diff[halfn+a, halfn+b] += abs(image0[x+i][y+j] - image1[x+i+a][y+j+b])
+
+    i = 0
+    cv2.imwrite(
+        '/Users/illaria/BSUIR/Diploma/code/MediaPipeTry1/difference' + '.jpg',
+        diff)
+    print(np.matrix(diff))
+
+    cap.release()
+    coordinates_of_min = np.unravel_index(np.argmin(diff, axis=None), diff.shape)
+    new_x, new_y = x0 - (halfn - coordinates_of_min[0]), y0 - (halfn - coordinates_of_min[1])
+    print(coordinates_of_min)
+    print(new_x, new_y)
+    return new_x, new_y
 
 
 def main():
@@ -116,9 +194,13 @@ def main():
     while True:
         ret, frame = cap.read()
 
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        _, _, frame = cv2.split(hsv)
+
         # To improve performance
         frame.flags.writeable = False
 
+        # frame = Contrast(frame)
         results = eyeDetector.get_face_mesh_results(frame)
 
         # Make it writeable again
@@ -131,19 +213,19 @@ def main():
         frame_right_eye = eyeDetector.right_eye.draw(frame)
         result_frame = eyeDetector.left_eye.draw(frame_right_eye)
 
-        #result_frame = cv2.flip(result_frame, 1)
+        # result_frame = cv2.flip(result_frame, 1)
         # Show the image
         cv2.imshow('eyes', result_frame)
 
         # result_frame = result_frame[eyeDetector.right_eye.outside_y-30:eyeDetector.right_eye.outside_y+30,
         #                eyeDetector.right_eye.outside_x-10:eyeDetector.right_eye.outside_x+10+eye_difference]
-        result_frame = result_frame[eyeDetector.left_eye.outside_y - 30:eyeDetector.left_eye.outside_y + 30,
-                       eyeDetector.left_eye.outside_x - 20 -eye_left_delta_x:eyeDetector.left_eye.outside_x + 15]
+        result_frame = result_frame[eyeDetector.left_eye.outside_y - 40:eyeDetector.left_eye.outside_y + 20,
+                       eyeDetector.left_eye.outside_x - 20 - eye_left_delta_x:eyeDetector.left_eye.outside_x + 15]
         cv2.imshow('eyes difference', result_frame)
-        cv2.imwrite('/Users/illaria/BSUIR/Diploma/code/MediaPipeTry1/left_eye3/result' + str(i) + '.jpg', result_frame)
-        i += 1
-        if i == 20:
-            break
+        # cv2.imwrite('/Users/illaria/BSUIR/Diploma/code/MediaPipeTry1/left_eye_v_without_glasses1/result' + str(i) + '.jpg', result_frame)
+        # i += 1
+        # if i == 20:
+        #     break
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
@@ -151,4 +233,20 @@ def main():
     cv2.destroyAllWindows()
 
 
-main()
+# main()
+# Contrast()
+Fixate()
+
+# cap = cv2.VideoCapture(0)
+# while True:
+#     ret, frame = cap.read()
+#     # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     # ret, frame = cv2.threshold(frame, 127, 255, 0)
+#     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+#     _, _, frame = cv2.split(hsv)
+#     cv2.imshow('eyes', frame)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+#
+# cap.release()
+# cv2.destroyWindow()
